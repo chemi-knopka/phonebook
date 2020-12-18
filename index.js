ea80ef90d622e98c99b1ff831bd36895558573b9
shotius <sh.archemashvili@gmail.com>
2020-12-18 20:37:48 +0400

    step2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
     {
         id: 1,
         name: "artos",
@@ -40,6 +40,19 @@ app.get('/api/persons/:id', (req, res) => {
     }
 
     res.json(person)
+});
+
+// delete person
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if (!person){
+        res.status(404).end()
+    }
+
+    persons = persons.filter(person => person.id !== id);
+    res.send(`deleted person with id : ${id}`)
 })
 
 const PORT = 3001
