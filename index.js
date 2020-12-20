79ad9ebe3ac1e0353e6d8eb609557132918f7975
shotius <sh.archemashvili@gmail.com>
2020-12-21 01:46:45 +0400

    changed port declaration

--- index.js
+++ index.js
@@ -100,6 +100,6 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`server is listening on port ${PORT}`);
