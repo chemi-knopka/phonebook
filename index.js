d6b9927c2014a73afc2d858229dade7321839178
shotius <sh.archemashvili@gmail.com>
2021-01-20 20:15:00 +0400

    3.22

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
-const express = require("express");
+const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const cors = require('cors');
-const Contact = require('./models/contacts')
+const Contact = require('./models/contacts');
 
 
-app.use(express.static('build'))
-app.use(cors())
+app.use(express.static('build'));
+app.use(cors());
 app.use(express.json());
 
 // set up morgan for login outputs
@@ -16,104 +16,104 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 // -- routes ---
 app.get('/api/persons', (req, res) => {
-    Contact.find({}).then(contacts => {
-        res.json(contacts)
-    })
+  Contact.find({}).then(contacts => {
+    res.json(contacts);
+  });
 });
 
 app.get('/info', async (req, res, next) => {
-    Contact.find({}).then(allContacts => {
-        const personInfo = `<p>there are ${allContacts.length} persons in the phonephone</p>
-        <p> ${new Date} </p>`
+  Contact.find({}).then(allContacts => {
+    const personInfo = `<p>there are ${allContacts.length} persons in the phonephone</p>
+        <p> ${new Date} </p>`;
             
-        res.send(personInfo)
-    })
+    res.send(personInfo);
+  });
     
     
 });
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Contact.findById(req.params.id)
-        .then(contact => {
-            if (contact){
-                res.json(contact)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => {
-            //res.status(400).send({error: "malformed id"})
-            next(error)
-        })
+  Contact.findById(req.params.id)
+    .then(contact => {
+      if (contact){
+        res.json(contact);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(error => {
+      //res.status(400).send({error: "malformed id"})
+      next(error);
+    });
 });
 
 // delete person
 app.delete('/api/persons/:id', (request, response, next) => {
-    Contact.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
+  Contact.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
+});
 
 //post new person
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body;
     
-    // check if both name and phone number are provided
-    if (!body.name || !body.number){
-        res.status(400).json({
-            error: "name or number is not provided"
-        })
-    }
+  // check if both name and phone number are provided
+  if (!body.name || !body.number){
+    res.status(400).json({
+      error: 'name or number is not provided'
+    });
+  }
     
-    // create new contact to add in db
-    const contact = new Contact({
-        name: body.name,
-        number: body.number,
-    })
+  // create new contact to add in db
+  const contact = new Contact({
+    name: body.name,
+    number: body.number,
+  });
 
-    // save contact in db
-    contact
-        .save()
-        .then(returnedContact => res.json(returnedContact))
-        .catch(error => next(error))
+  // save contact in db
+  contact
+    .save()
+    .then(returnedContact => res.json(returnedContact))
+    .catch(error => next(error));
 });
 
 // update person
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body;
+  const body = request.body;
     
-    // this is for validation check
-    // it must be enabled manualy while update
-    const opts = { runValidators: true,  new: true  };
-    Contact
-        .findByIdAndUpdate(request.params.id, {number: body.number}, opts)
-        .then(updatedCont => {
-        response.json(updatedCont.toJSON())
-        })
-        .catch(error => next(error))
-})
+  // this is for validation check
+  // it must be enabled manualy while update
+  const opts = { runValidators: true,  new: true  };
+  Contact
+    .findByIdAndUpdate(request.params.id, {number: body.number}, opts)
+    .then(updatedCont => {
+      response.json(updatedCont.toJSON());
+    })
+    .catch(error => next(error));
+});
 
 // this middleware must be in the end of the code to work correctly
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
   // catch CastError, it happens when provied bad id
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if(error.name === "ValidationError"){
-      return response.status(400).send({ error: error.message})
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if(error.name === 'ValidationError'){
+    return response.status(400).send({ error: error.message});
   }
 
-  next(error)
-}
+  next(error);
+};
 
 // use error handle middle ware in routes
-app.use(errorHandler)
+app.use(errorHandler);
 
 
 // listen to the port
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
 console.log(`server is listening on port ${PORT}`);
--- models/contacts.js
+++ models/contacts.js
@@ -1,38 +1,38 @@
 const mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator'); 
 
-const url = process.env.DB_URI
-console.log("connecting to database....");
+const url = process.env.DB_URI;
+console.log('connecting to database....');
 
 // database connection
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(res => {
-        console.log("connected to MongoDB");
-    })
-    .catch(error => {
-        console.log("failed to connect to db", error.message)
-    })
+  .then(res => {
+    console.log('connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('failed to connect to db', error.message);
+  });
 
 // define schema for models
 const contactSchema = mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        unique: true
-    },
-    number:{
-        type:String,
-        minLength: 8
-    }
-})
+  name: {
+    type: String,
+    minLength: 3,
+    unique: true
+  },
+  number:{
+    type:String,
+    minLength: 8
+  }
+});
 
 contactSchema.set('toJSON', {
-    transform: (document, contactObj) => {
-        contactObj.id = contactObj._id
-        delete contactObj._id
-        delete contactObj.__v
-    }
-})
+  transform: (document, contactObj) => {
+    contactObj.id = contactObj._id;
+    delete contactObj._id;
+    delete contactObj.__v;
+  }
+});
 
 // Apply the uniqueValidator plugin to userSchema.
 contactSchema.plugin(uniqueValidator);
